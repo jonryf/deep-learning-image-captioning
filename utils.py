d3df32afa08a3e69ee98d3782b7b468145b41b25
root <root@DESKTOP-OR6I109.localdomain>
2020-02-24 10:03:57 -0800

    added softmax with temperature util function

--- utils.py
+++ utils.py
@@ -6,3 +6,13 @@ def get_device():
         return torch.device("cuda")
     else:
         return torch.device("cpu")
+
+# gets softmax of each row with temperature
+# subtracts max from every element in each row, keps ordering but plays with ratios of solutions
+def softmax(x, temp=1.0):
+    values = (((x.T - torch.max(x, axis=1)[0]).T) / temp).float()
+    exp = torch.exp(values).float()
+    sM = (exp.T / torch.sum(exp, axis=1)).T
+    sM[torch.where(sM == 0)] = 0.0000001
+    sM[torch.where(sM == 1)] = 0.9999999
+    return sM
