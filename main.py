32041ea7e35a14dc8b0d412e7aef2ace38fb7f28
Alex Polus <alex.polus13@gmail.com>
2020-02-24 12:39:30 -0800

    Added id parser

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from models import Encoder, Decoder
 from runner import Runner
 from settings import BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS, IMAGES_DIR, CAPTIONS_DIR
 from evaluate_captions import evaluate_captions
+import pandas
+import numpy as np
+
+
+def process_csv(train=True):
+    if train:
+        path = './TrainImageIds.csv'
+    else:
+        path = './TestImageIds.csv'
+    df = pandas.read_csv(path, header=None)
+    df = np.array(df)
+    print("Training set = ", train)
+    print("# Images: ", df.size)
+    return np.array(df)
 
 
 def show_options():
@@ -13,10 +27,27 @@ def show_options():
     print("(5): Pre=trained word embeddings")
     print("(q): quit program")
 
+
 def task_4_1(): # training and validation loss for LSTM and Vanilla RNN
-    pass
+
+    training_ids = process_csv(True)
+    testing_ids = process_csv(False)
+
+    # load data and transform images
+    train_dataset = get_loader(IMAGES_DIR + "/train/", CAPTIONS_DIR + "/train/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
+    val_dataset = get_loader(IMAGES_DIR + "/val/", CAPTIONS_DIR + "/val/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
+    test_dataset = get_loader(IMAGES_DIR + "/test/", CAPTIONS_DIR + "/test/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
+
+    encoder = Encoder(2048)
+    decoder = Decoder(2048, 1024, len(train_dataset.dataset.vocab.word2idx))
+
+    runner = Runner(encoder, decoder, train_dataset, val_dataset, test_dataset)
+
+    runner.train()
 def task_4_2(): # Cross Entropy and Perplexity score on test set
     pass
+
+
 def task_4_3(): # BLEU-1 and BLEU-4 scores on deterministic LSTM and Vanilla RNN
     true_captions_path = './'
 
@@ -32,23 +63,17 @@ def task_4_3(): # BLEU-1 and BLEU-4 scores on deterministic LSTM and Vanilla RNN
     print("BLEU-1 Score:", b1)
     print("BLEU-4 Score:", b4)
 
+
 def task_4_4(): # Experiment with temperatures
     pass
+
+
 def task_4_5(): # Pre-trained word embeddings
     pass
 
+
 if __name__ == "__main__":
-    # load data and transform images
-    train_dataset = get_loader(IMAGES_DIR + "/train/", CAPTIONS_DIR + "/train/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
-    val_dataset = get_loader(IMAGES_DIR + "/val/", CAPTIONS_DIR + "/val/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
-    test_dataset = get_loader(IMAGES_DIR + "/test/", CAPTIONS_DIR + "/test/", BATCH_SIZE, SHUFFLE_DATA, NUM_WORKERS)
 
-    encoder = Encoder(2048)
-    decoder = Decoder(2048, 1024, len(train_dataset.dataset.vocab.word2idx))
-
-    runner = Runner(encoder, decoder, train_dataset, val_dataset, test_dataset)
-
-    runner.train()
     show_options()
     i = ""
     while i != 'q':
